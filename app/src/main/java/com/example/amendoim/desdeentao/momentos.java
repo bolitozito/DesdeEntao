50b3fa6330d408ea0769992b598dcfdb7f2d4548
bolitozito <amendoinsanda@gmail.com>
2026-06-19 05:21:47 -0300

    2 páginas feitas

--- app/src/main/java/com/example/amendoim/desdeentao/momentos.java
+++ app/src/main/java/com/example/amendoim/desdeentao/momentos.java
@@ -2,6 +2,7 @@ package com.example.amendoim.desdeentao;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class momentos extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class momentos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_momentos);
+    }
+
+
+    public void onVoltar(View view) {
+        finish();
     }
 }
