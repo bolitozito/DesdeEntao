8a5faba82629c88b815d706c4d1db8a396e3e987
bolitozito <amendoinsanda@gmail.com>
2026-07-18 02:14:20 -0300

    Indo para momentos | Travamento resolvido

--- app/src/main/java/com/example/amendoim/desdeentao/Home.java
+++ app/src/main/java/com/example/amendoim/desdeentao/Home.java
@@ -21,6 +21,7 @@ public class Home extends Fragment {
     TextView txt2;
     TextView txt3;
     TextView txtCont;
+    TextView txtMensagem;
 
     public Home() {
     }
@@ -34,6 +35,7 @@ public class Home extends Fragment {
         txt1 = view.findViewById(R.id.txt1);
         txt2 = view.findViewById(R.id.txt2);
         txt3 = view.findViewById(R.id.txt3);
+        txtMensagem = view.findViewById(R.id.txtMensagem);
         txtCont = view.findViewById(R.id.txtCont);
 
         Calendar dataInicio = Calendar.getInstance();
@@ -47,10 +49,44 @@ public class Home extends Fragment {
         txtCont.setText(String.valueOf(dias));
 
         Typeface fonte = Typeface.createFromAsset(getActivity().getAssets(), "fonts/dancing.ttf");
+        Typeface fonte1 = Typeface.createFromAsset(getActivity().getAssets(), "fonts/hoollidday.ttf");
+        Typeface fonte2 = Typeface.createFromAsset(getActivity().getAssets(), "fonts/poppins.regular.ttf");
         txt1.setTypeface(fonte);
-        txt2.setTypeface(fonte);
-        txt3.setTypeface(fonte);
+        txt2.setTypeface(fonte1);
+        txt3.setTypeface(fonte1);
+        txtMensagem.setTypeface(fonte2);
 
+
+        String[] mensagens = {
+
+                "❝ Bom dia! Espero que hoje você sorria bastante.💜 ❞",
+
+                "❝ Se hoje estiver difícil, lembra que alguém torce muito por você. ❞",
+
+                "❝ Você passou pela minha cabeça hoje... como em todos os outros dias. ❞",
+
+                "❝ Espero que hoje você encontre um motivo para rir. ❞",
+
+                "❝ Você merece tudo de bom que esse mundo pode oferecer. ❞",
+
+                "❝ Mesmo longe, estou pensando em você. ❞",
+
+                "❝ Espero que essa mensagem tenha conseguido arrancar um sorrisinho seu. ❞",
+
+                "❝ Você é uma das melhores partes dos meus dias. ❞",
+
+                "❝ Hoje é mais um dia em que gosto de você. ❞",
+
+                "❝ Você consegue tudo o que colocar na cabeça. Eu acredito em você. ❞"
+
+        };
+
+        Calendar calendar = Calendar.getInstance();
+
+        int dia = calendar.get(Calendar.DAY_OF_YEAR);
+        int indice = dia % mensagens.length;
+
+        txtMensagem.setText(mensagens[indice]);
         return view;
     }
 
